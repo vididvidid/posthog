0c07821e0aabc84adf1f2b721da8af61b2ac4b0e
Yasen Slavov <yasen@posthog.com>
2025-08-11 17:16:23 +0300

    Add PersonalAPIKey logging on the BE

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -4582,6 +4582,7 @@ export enum ActivityScope {
     BATCH_IMPORT = 'BatchImport',
     FEATURE_FLAG = 'FeatureFlag',
     PERSON = 'Person',
+    PERSONAL_API_KEY = 'PersonalAPIKey',
     GROUP = 'Group',
     INSIGHT = 'Insight',
     PLUGIN = 'Plugin',
