a58701f7d3dc2c9bdbef93492949e936ebc78975
Yasen Slavov <yasen@posthog.com>
2025-08-12 15:33:27 +0300

    Change how we present the logs

--- posthog/models/activity_logging/activity_log.py
+++ posthog/models/activity_logging/activity_log.py
@@ -408,6 +408,7 @@ field_exclusions: dict[ActivityScope, list[str]] = {
         "value",
         "secure_value",
         "last_used_at",
+        "last_rolled_at",
     ],
     "User": [
         "password",
